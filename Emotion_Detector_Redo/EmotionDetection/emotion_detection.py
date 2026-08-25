22e8fcd1f40b72e37223614190bba6ab4dc08ac8
Taylor Ng'andwe <xcid1507@gmail.com>
2026-08-26 00:56:05 +0100

    Align detector with course-required implementation

--- Emotion_Detector_Redo/EmotionDetection/emotion_detection.py
+++ Emotion_Detector_Redo/EmotionDetection/emotion_detection.py
@@ -1,11 +1,16 @@
+import json
 import requests
 
 URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-HEADERS = {'grpc-metadata-mm-model-id': 'emotion_aggregated-workflow_lang_en_stock'}
+
+HEADERS = {
+    'grpc-metadata-mm-model-id': 'emotion_aggregated-workflow_lang_en_stock'
+}
 
 
-def emotion_detector(text_to_analyze):
+def emotion_detector(text_to_analyze: str | None) -> dict:
     payload = {'raw_document': {'text': text_to_analyze}}
+
     response = requests.post(url=URL, headers=HEADERS, json=payload)
 
     if response.status_code == 400:
@@ -15,12 +20,22 @@ def emotion_detector(text_to_analyze):
             'fear': None,
             'joy': None,
             'sadness': None,
-            'dominant_emotion': None
+            'dominant_emotion': None,
         }
 
-    response_dict = response.json()
-    emotions = response_dict['emotionPredictions'][0]['emotion']
+    json_response = json.loads(response.text)
 
-    dominant_emotion = max(emotions, key=emotions.get)
-    emotions['dominant_emotion'] = dominant_emotion
-    return emotions
+    emotions = json_response.get('emotionPredictions')[0] or {}
+    emotions_rating = emotions.get('emotion')
+
+    dominant_emotion = ''
+    highest_score = 0
+
+    for emotion_key, emotion_value in emotions_rating.items():
+        if emotion_value > highest_score:
+            highest_score = emotion_value
+            dominant_emotion = emotion_key
+
+    emotions_rating.update({'dominant_emotion': dominant_emotion})
+
+    return emotions_rating
